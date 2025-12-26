3ea931a451a0c2394302776e60c3e51c59fae2cd
Fernando Falcone <C0647451@vale.com>
2025-12-26 18:29:34 -0300

    feat: agrega comentarios para guiar la implementación del código y la estructura de enrutamiento

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Home } from './Home';
 
 function App() {
   return (
+    //Observación: Mover las rutas a un archivo routes.ts o main.tsx según convenga y despues importarlas aquí. Hay una diferencia entre las dos formas de hacerlo. INVESTIGAR.
     <BrowserRouter>
         <Link to="/card" target="_blank">
           <button>Página Card</button>
@@ -17,6 +18,12 @@ function App() {
           <Route path='/card' element={<Card/>}/>
         </Routes>
     </BrowserRouter>
+    //1. Agregar un componente Header con enlaces de navegación a las páginas Home y Cart
+    //2. Crear una sección hero en Home con un mensaje de bienvenida y un campo de búsqueda con botón para buscar libros
+    //3. Crear un diseño de cuadrícula para mostrar tarjetas de libros en Home
+    //4. Cada tarjeta debe mostrar la imagen, título, autor, precio y un botón "Agregar al carrito"
+    //5. Agregar un componente de paginación al final de Home para navegar entre varias páginas de libros
+    //6. Agregar un footer con enlaces a redes sociales e información de contacto...
   )
 }
 
