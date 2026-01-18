159805e31468df96d85c9cb69183f2b3a40dac65
Lupe García <ggvalero@idealista.com>
2026-01-18 12:24:36 +0100

    fix: fix fetch function

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState, type ReactNode } from 'react';
+import React, { useEffect, useState} from 'react';
 import Card, { type CardProps } from './components/Card';
 import { Hero } from './components/Hero';
+import type { BookList } from './types/Book';
+import { fetchBooks, fetchByBookId } from './services/api';
 
 const handleCardButton = () => console.log('pepe');
 const hable1Button = () => alert('hola')
@@ -28,61 +30,22 @@ const cards:  CardProps[] = [
 export function Home({
 
 }: HomeProps){
-    //const {data, loading} = searchBooks('https://www.googleapis.com/books/v1/volumes?q={search}');
-    const url = 'https://www.googleapis.com/books/v1/volumes?q=react';
-    /* Para tipar el estado data se puede usar generics en useState<>
-     ejemplo: useState<Product[]>([]) esto indica que data es un array de productos y se va inicializar como un array vacío
-     Generics es una forma de definir tipos en TypeScript que son dinámicos y se pueden reutilizar en diferentes contextos.
-    Puedes entenderlo más lo que es en la documentación oficial: https://www.typescriptlang.org/docs/handbook/2/generics.html */
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+   const [data, setData] = useState<BookList| null>();
 
-    const handleClick = ()=> {console.log('button check')}
+   useEffect(()=> {
+        fetchBooks();
+        //setData(data)
+   }, [])
 
-    //FORMA BÁSICA DE HACER FETCH DENTRO DE UN useEffect
-    useEffect(()=> {
-        //No es necesario setLoading(true) aquí porque ya se inicializa en true
-        setLoading(true);
-        fetch(url)
-        .then(response => (response.json()))
-        //para aceder a los libros es data.items que es un array de Book
-        .then(data => setData(data))
-        //método de fetch que se usa cuando se ha terminado de hacer todas las llamadas
-        .finally(()=> setLoading(false))
-    }, [])
-
-    //PADRÓN DE DISEÑO CAPA DE SERVICIO API
-    //Una forma alternativa de obtener los datos sería moviendo la lógica de fetch a una función asíncrona (por ejemplo en el archivo api.ts) y llamándola dentro de useEffect
-    //Así como Promise, async/await es otra forma de manejar operaciones asíncronas en JavaScript.
-    // useEffect(() => {
-    // const loadData = async () => {
-    //   try {
-    //     const data = await fetchProducts();
-    //     setProducts(data);
-    //   } catch (err) {
-    //     setError(err.message);
-    //   }
-    // };
-
-    // loadData();
-    // }, []);
-
-    //CUSTOM HOOK PARA FETCH
-    //Otra forma sería creando un custom hook (por ejemplo useSearchProducts que está en src/hooks/useSearchProducts.ts) que encapsule la lógica de fetch y devuelva los datos y el estado de carga.
-    // Uno estado para cuando el usuario escribe en el input de búsqueda
-    // const [searchQuery, setSearchQuery] = useState('');
-    // const { data, loading } = useSearchProducts(searchQuery);
-    // const handleSearch = (value: string) => {
-    //     setSearchQuery(value);
-    // };
-
-
+   const htmlData = data.map((item: BookList)=> {
+        return <li>{item.kind}</li>
+   })
+   console.log(htmlData)
     return (
         <>
-        <Hero title='Page title' label='Find your book' text='search' placeholder='Find your book' onClick={handleClick}/>
-        {loading && <li>Loading...</li>}
+        <Hero title='Page title' label='Find your book' text='search' placeholder='Find your book' onClick={handleCardButton}/>
         {/* Despues que ya tienes el tipo, puedes mapear los datos correctamente con las propiedades que tiene el tipo Book */}
-        {data.map(book )}
+        {/* //{htmlData} */}
          <section className='grid grid-cols-4 gap-4'>
             {/* Aqui ya puedes modificar el tipo de CardProps para que acepte las propriedades venientes de Book */}
             {cards.map((elem: CardProps) =>(
--- src/services/api.ts
+++ src/services/api.ts
@@ -5,25 +5,53 @@
 //1. Centralización de la lógica de llamadas a la API, lo que facilita el mantenimiento y las actualizaciones.
 //2. Reutilización de código en diferentes partes de la aplicación.
 //3. Mejora de la legibilidad del código al abstraer los detalles de las llamadas a la API.
-import { ProductList } from '../types/Product';
-import { Product } from '../types/Product';
+import type { BookList } from '../types/Book';
 
-// Define la URL base de la API (lo que normalmente se repite en todas las llamadas)
-const BASE_URL = 'https://api.example.com';
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
 
-// Función para obtener un listado de productos desde la API (en nuestro caso libros y enviamos un parámetro de búsqueda para que nos devuelva una lista filtrada de libros)
-export const fetchProducts = async (): Promise<ProductList> => {
-  const response = await fetch(`${BASE_URL}/products`);
+export async function fetchBooks(): Promise<BookList> {
+    const response = await fetch(`${BASE_URL}?q=react`);
+
+    if(!response.ok) {
+        throw new Error(`Response status: ${response.status}`);    }
+
+    const ListResult = await response.json();
+    return ListResult;
+}
+
+export async function fetchByBookId(id: string): Promise<BookList> {
+    const response = await fetch(`${BASE_URL}/books/${id}`);
+    if(!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+    }
+
+    const bookResult = await response.json();
+    return bookResult;
+}
+
+
+
+
+
+// import { ProductList } from '../types/Product';
+// import { Product } from '../types/Product';
+
+// // Define la URL base de la API (lo que normalmente se repite en todas las llamadas)
+// const BASE_URL = 'https://api.example.com';
+
+// // Función para obtener un listado de productos desde la API (en nuestro caso libros y enviamos un parámetro de búsqueda para que nos devuelva una lista filtrada de libros)
+// export const fetchProducts = async (): Promise<ProductList> => {
+//   const response = await fetch(`${BASE_URL}/products`);
   
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
+//   if (!response.ok) {
+//     throw new Error('Network response was not ok');
+//   }
   
-  return response.json();
-};
+//   return response.json();
+// };
 
-// Función para obtener un producto específico por su ID (en nuestro caso un libro)
-export const fetchProductById = async (id): Promise<Product> => {
-  const response = await fetch(`${BASE_URL}/products/${id}`);
-  return response.json();
-};
+// // Función para obtener un producto específico por su ID (en nuestro caso un libro)
+// export const fetchProductById = async (id): Promise<Product> => {
+//   const response = await fetch(`${BASE_URL}/products/${id}`);
+//   return response.json();
+// };
